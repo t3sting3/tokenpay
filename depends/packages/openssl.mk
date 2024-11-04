46960c95a4cf8f577708197de4e3c9178f0fec44
sunerok <justinvforvendetta@gmail.com>
2024-11-04 13:52:10 -0500

    Revert "if this doesnt compile, go back and unlink ssl from qt"
    This reverts commit a8b0d40777ce105786ccfbfde80cfa279aefd11d.

--- depends/packages/openssl.mk
+++ depends/packages/openssl.mk
@@ -1,3 +1,4 @@
+
 package=openssl
 $(package)_version=1.1.1l
 $(package)_download_path=https://github.com/openssl/openssl/archive/refs/tags
@@ -46,11 +47,9 @@ $(package)_config_opts_x86_64_darwin=darwin64-x86_64-cc
 $(package)_config_opts_aarch64_darwin=darwin64-arm64-cc
 $(package)_config_opts_x86_64_mingw32=mingw64
 $(package)_config_opts_i686_mingw32=mingw
-endef
-
-define $(package)_preprocess_cmds
-  sed -i.old "/define DATE/d" util/mkbuildinf.pl && \
-  sed -i.old "s|engines apps test|engines|" Makefile.org
+ifeq ($(build),darwin)
+$(package)_config_opts:=$(subst -arch $(arch),,$(package)_config_opts)
+endif
 endef
 
 define $(package)_config_cmds
@@ -58,13 +57,9 @@ define $(package)_config_cmds
 endef
 
 define $(package)_build_cmds
-  $(MAKE) -j1 build_libs libcrypto.pc libssl.pc openssl.pc
+  $(MAKE) -j1 build_libs
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) INSTALL_PREFIX=$($(package)_staging_dir) -j1 install_sw
-endef
-
-define $(package)_postprocess_cmds
-  rm -rf share bin etc
+  $(MAKE) DESTDIR=$($(package)_staging_dir) -j1 install_dev
 endef
