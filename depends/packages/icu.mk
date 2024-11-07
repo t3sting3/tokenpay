1d6d6053e31d031fbb1a958454d79f0711a2e17a
sunerok <justinvforvendetta@gmail.com>
2024-11-07 11:47:27 -0500

    bump icu up

--- depends/packages/icu.mk
+++ depends/packages/icu.mk
@@ -1,39 +1,45 @@
 package=icu
-$(package)_version=58.3
-$(package)_download_path=https://github.com/unicode-org/icu/releases/download/release-58-3/
-$(package)_file_name=icu4c-58_3-src.tgz
+$(package)_version=63_2
+$(package)_download_path=https://github.com/unicode-org/icu/releases/download/release-63-2/
+$(package)_file_name=$(package)4c-$($(package)_version)-src.tgz
+$(package)_sha256_hash=4671e985b5c11252bff3c2374ab84fd73c609f2603bb6eb23b8b154c69ea4215
 $(package)_build_subdir=source
-
-
-$(package)_patch_xlocale = sed -i 's|xlocale[.]h|locale.h|' i18n/digitlst.cpp
-$(package)_reverse_patch_xlocale_linux = true
-$(package)_reverse_patch_xlocale_mingw32 = true
-$(package)_reverse_patch_xlocale_darwin = sed -i 's|locale[.]h|xlocale.h|' i18n/digitlst.cpp
-$(package)_reverse_patch_xlocale =  $($(package)_reverse_patch_xlocale_$(host_os))
-
+$(package)_standard_opts=--disable-extras --disable-strict --enable-static --disable-shared --disable-tests --disable-samples --disable-dyload --disable-layoutex
 
 define $(package)_set_vars
-$(package)_config_opts=
+  $(package)_config_opts=$($(package)_standard_opts)
+  $(package)_config_opts_debug=--enable-debug --disable-release
+  $(package)_config_opts_release=--disable-debug --enable-release
+  $(package)_config_opts_mingw32=--with-cross-build="$($(package)_extract_dir)/build"
+  $(package)_config_opts_darwin=--with-cross-build="$($(package)_extract_dir)/build" LIBTOOL="$($(package)_libtool)"
+  $(package)_archiver_darwin=$($(package)_libtool)
+  $(package)_cflags_linux=-fPIC
+  $(package)_cppflags_linux=-fPIC
+  $(package)_cxxflags=-std=c++11
+endef
+
+define $(package)_preprocess_cmds
+  PKG_CONFIG_SYSROOT_DIR=/ \
+  PKG_CONFIG_LIBDIR=$(host_prefix)/lib/pkgconfig \
+  PKG_CONFIG_PATH=$(host_prefix)/share/pkgconfig \
+  mkdir -p build && cd build && \
+  ../source/runConfigureICU Linux $($(package)_standard_opts) CXXFLAGS=-std=c++11 && \
+  $(MAKE) && cd ..
 endef
 
 define $(package)_config_cmds
-  $($(package)_patch_xlocale) && \
-  mkdir native && cd native && \
-  PATH=$(or $(HOST_NATIVE_PATH),${PATH}) ../configure && \
-  PATH=$(or $(HOST_NATIVE_PATH),${PATH}) make -j 10 && cd .. && \
-  $($(package)_reverse_patch_xlocale) && \
-  PATH=${PATH} $($(package)_autoconf) --with-cross-build=$($(package)_build_dir)/native --prefix=$(host_prefix) --enable-extras=no \
-  --enable-strict=no -enable-shared=yes --enable-tests=no \
-  --enable-samples=no --enable-dyload=no --enable-tools=no CXXFLAGS="--std=c++11"
+  PKG_CONFIG_SYSROOT_DIR=/ \
+  PKG_CONFIG_LIBDIR=$(host_prefix)/lib/pkgconfig \
+  PKG_CONFIG_PATH=$(host_prefix)/share/pkgconfig \
+  sed -i.old 's|^GEN_DEPS.c=.*|& $($(package)_cflags)|' config/mh-mingw* && \
+  sed -i.old 's|^GEN_DEPS.cc=.*|& $($(package)_cxxflags)|' config/mh-mingw* && \
+  $($(package)_autoconf)
 endef
 
 define $(package)_build_cmds
-  sed -i.old 's|gCLocale = _create_locale(LC_ALL, "C");|gCLocale = NULL;|' i18n/digitlst.cpp && \
-  sed -i.old "s|freelocale(gCLocale);||" i18n/digitlst.cpp && \
-  PATH=${PATH} $(MAKE)
+  $(MAKE)
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) install $($(package)_build_opts)
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install
 endef
-
