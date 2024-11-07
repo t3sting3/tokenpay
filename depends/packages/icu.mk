2dc8d48eb195c46f7168dfe058992a1b299a66f3
sunerok <justinvforvendetta@gmail.com>
2024-11-07 13:38:57 -0500

    icu pre-bug

--- depends/packages/icu.mk
+++ depends/packages/icu.mk
@@ -1,45 +1,32 @@
-package=icu
-$(package)_version=63_2
-$(package)_download_path=https://github.com/unicode-org/icu/releases/download/release-63-2/
-$(package)_file_name=$(package)4c-$($(package)_version)-src.tgz
-$(package)_sha256_hash=4671e985b5c11252bff3c2374ab84fd73c609f2603bb6eb23b8b154c69ea4215
-$(package)_build_subdir=source
-$(package)_standard_opts=--disable-extras --disable-strict --enable-static --disable-shared --disable-tests --disable-samples --disable-dyload --disable-layoutex
+package=icu4c
+$(package)_version=55.2
+$(package)_download_path=https://github.com/unicode-org/icu/releases/download/release-55-2/
+$(package)_file_name=$(package)-55_2-src.tgz
+$(package)_sha256_hash=eda2aa9f9c787748a2e2d310590720ca8bcc6252adf6b4cfb03b65bef9d66759
+$(package)_patches=icu-001-dont-build-static-dynamic-twice.patch
 
 define $(package)_set_vars
-  $(package)_config_opts=$($(package)_standard_opts)
-  $(package)_config_opts_debug=--enable-debug --disable-release
-  $(package)_config_opts_release=--disable-debug --enable-release
-  $(package)_config_opts_mingw32=--with-cross-build="$($(package)_extract_dir)/build"
-  $(package)_config_opts_darwin=--with-cross-build="$($(package)_extract_dir)/build" LIBTOOL="$($(package)_libtool)"
-  $(package)_archiver_darwin=$($(package)_libtool)
-  $(package)_cflags_linux=-fPIC
-  $(package)_cppflags_linux=-fPIC
-  $(package)_cxxflags=-std=c++11
-endef
-
-define $(package)_preprocess_cmds
-  PKG_CONFIG_SYSROOT_DIR=/ \
-  PKG_CONFIG_LIBDIR=$(host_prefix)/lib/pkgconfig \
-  PKG_CONFIG_PATH=$(host_prefix)/share/pkgconfig \
-  mkdir -p build && cd build && \
-  ../source/runConfigureICU Linux $($(package)_standard_opts) CXXFLAGS=-std=c++11 && \
-  $(MAKE) && cd ..
+  $(package)_build_opts=CFLAGS="$($(package)_cflags) $($(package)_cppflags) -DU_USING_ICU_NAMESPACE=0 -DU_STATIC_IMPLEMENTATION -DU_COMBINED_IMPLEMENTATION -fPIC -DENABLE_STATIC=YES -DPGKDATA_MODE=static"
 endef
 
 define $(package)_config_cmds
-  PKG_CONFIG_SYSROOT_DIR=/ \
-  PKG_CONFIG_LIBDIR=$(host_prefix)/lib/pkgconfig \
-  PKG_CONFIG_PATH=$(host_prefix)/share/pkgconfig \
-  sed -i.old 's|^GEN_DEPS.c=.*|& $($(package)_cflags)|' config/mh-mingw* && \
-  sed -i.old 's|^GEN_DEPS.cc=.*|& $($(package)_cxxflags)|' config/mh-mingw* && \
-  $($(package)_autoconf)
+  patch -p1 < $($(package)_patch_dir)/icu-001-dont-build-static-dynamic-twice.patch &&\
+  mkdir builda &&\
+  mkdir buildb &&\
+  cd builda &&\
+  sh ../source/runConfigureICU Linux &&\
+  make &&\
+  cd ../buildb &&\
+  sh ../source/runConfigureICU MinGW --enable-static=yes --disable-shared --disable-layout --disable-layoutex --disable-tests --disable-samples --prefix=$(host_prefix) --with-cross-build=`pwd`/../builda &&\
+  $(MAKE) $($(package)_build_opts)
 endef
 
-define $(package)_build_cmds
-  $(MAKE)
-endef
+#define $(package)_build_cmds
+#  cd source &&\
+   $(MAKE) $($((package)_build_opts) `nproc`
+#endef
 
 define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+  cd buildb &&\
+  $(MAKE) $($(package)_build_opts) DESTDIR=$($(package)_staging_dir) install lib/*
 endef
