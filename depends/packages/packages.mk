a7a5afd70e09e8832c49edb19cdc6b51765df907
sunerok <justinvforvendetta@gmail.com>
2024-11-06 19:55:47 -0500

    new depends when was working

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,9 +1,11 @@
-packages:=boost openssl libevent libcap libseccomp zeromq
+packages:=boost openssl libevent zeromq libseccomp zeromq 
+native_packages := native_ccache
 
 qt_native_packages = native_protobuf
 qt_packages = qrencode protobuf zlib
 
-qt_linux_packages:=qt expat dbus libxcb libxcb_util xcb_proto libXau xorgproto freetype fontconfig libX11 xextproto libXext xtrans
+qt_x86_64_linux_packages:=qt expat dbus libxcb xcb_proto libXau xorgproto xproto freetype fontconfig libX11 xextproto libXext xtrans
+qt_i686_linux_packages:=$(qt_x86_64_linux_packages)
 
 qt_darwin_packages=qt
 qt_mingw32_packages=qt
@@ -12,8 +14,8 @@ wallet_packages=bdb
 
 upnp_packages=miniupnpc
 
-darwin_native_packages = native_ds_store native_mac_alias
+darwin_native_packages = native_biplist native_ds_store native_mac_alias
 
 ifneq ($(build_os),darwin)
-darwin_native_packages += native_cctools native_cdrkit native_libdmg-hfsplus native_libtapi
+darwin_native_packages += native_cctools native_cdrkit native_libdmg-hfsplus
 endif
