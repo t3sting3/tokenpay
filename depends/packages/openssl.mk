634c99e0001372fc72a19e1e1343b167d674b392
sunerok <8443713+justinvforvendetta@users.noreply.github.com>
2024-11-16 15:19:37 -0500

    Update openssl.mk

--- depends/packages/openssl.mk
+++ depends/packages/openssl.mk
@@ -78,6 +78,16 @@ $(package)_config_opts+=no-whirlpool
 #$(package)_config_opts+=no-zlib-dynamic
 $(package)_config_opts+=$($(package)_cflags) $($(package)_cppflags)
 $(package)_config_opts+=-DPURIFY
+$(package)_config_opts_x86_64_linux=linux-x86_64
+$(package)_config_opts_i686_linux=linux-generic32
+$(package)_config_opts_arm_linux=linux-generic32
+$(package)_config_opts_aarch64_linux=linux-generic64
+$(package)_config_opts_mipsel_linux=linux-generic32
+$(package)_config_opts_mips_linux=linux-generic32
+$(package)_config_opts_powerpc_linux=linux-generic32
+$(package)_config_opts_x86_64_darwin=darwin64-x86_64-cc
+$(package)_config_opts_x86_64_mingw32=mingw64
+$(package)_config_opts_i686_mingw32=mingw
 endef
 
 define $(package)_preprocess_cmds
