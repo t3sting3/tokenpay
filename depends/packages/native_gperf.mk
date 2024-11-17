51a9add3df1d2b22cc187559fb540e00718d2110
sunerok <8443713+justinvforvendetta@users.noreply.github.com>
2024-11-17 11:05:28 -0500

    update gperf

--- depends/packages/native_gperf.mk
+++ depends/packages/native_gperf.mk
@@ -1,8 +1,8 @@
 package=native_gperf
-$(package)_version=3.0.4
-$(package)_download_path=http://ftp.gnu.org/pub/gnu/gperf/
+$(package)_version=3.1
+$(package)_download_path=https://ftp.gnu.org/pub/gnu/gperf/
 $(package)_file_name=gperf-$($(package)_version).tar.gz
-$(package)_sha256_hash=767112a204407e62dbc3106647cf839ed544f3cf5d0f0523aaa2508623aad63e
+$(package)_sha256_hash=588546B945BBA4B70B6A3A616E80B4AB466E3F33024A352FC2198112CDBB3AE2
 
 define $(package)_set_vars
   $(package)_config_opts=--disable-manpages
