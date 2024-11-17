76cabc1da4d8d58a22d0de64304318d5529b82c9
sunerok <8443713+justinvforvendetta@users.noreply.github.com>
2024-11-17 10:56:58 -0500

    Update native_gperf.mk

--- depends/packages/native_gperf.mk
+++ depends/packages/native_gperf.mk
@@ -17,5 +17,5 @@ define $(package)_build_cmd
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) INSTALL_PREFIX=$($(package)_staging_dir) install
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install
 endef
