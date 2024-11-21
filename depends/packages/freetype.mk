f7f5ea2c2428e01aef9639aeaef600d9744426f3
sunerok <8443713+justinvforvendetta@users.noreply.github.com>
2024-11-21 17:51:18 -0500

    freetype

--- depends/packages/freetype.mk
+++ depends/packages/freetype.mk
@@ -1,18 +1,15 @@
 package=freetype
-$(package)_version=2.13.2
-$(package)_download_path=https://sourceforge.net/projects/freetype/files/freetype2/$($(package)_version)/
-$(package)_file_name=freetype-$($(package)_version).tar.xz
-$(package)_sha256_hash=12991c4e55c506dd7f9b765933e62fd2be2e06d421505d7950a132e4f1bb484d
+$(package)_version=2.7.1
+$(package)_download_path=http://download.savannah.gnu.org/releases/$(package)
+$(package)_file_name=$(package)-$($(package)_version).tar.bz2
+$(package)_sha256_hash=3a3bb2c4e15ffb433f2032f50a5b5a92558206822e22bfe8cbe339af4aa82f88
 
 define $(package)_set_vars
-  $(package)_config_opts  = --without-zlib --without-png --without-harfbuzz --without-bzip2 --enable-static --disable-shared
-  $(package)_config_opts += --enable-option-checking --without-brotli
-  $(package)_config_opts += --with-pic
+  $(package)_config_opts=--without-zlib --without-png --disable-static
+  $(package)_config_opts_linux=--with-pic
 endef
 
 define $(package)_config_cmds
-  printenv && \
-  echo "$($(package)_autoconf)" && \
   $($(package)_autoconf)
 endef
 
@@ -22,8 +19,4 @@ endef
 
 define $(package)_stage_cmds
   $(MAKE) DESTDIR=$($(package)_staging_dir) install
-endef
-
-define $(package)_postprocess_cmds
-  rm -rf share/man lib/*.la
 endef
