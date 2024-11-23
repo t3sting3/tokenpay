29ef39fa5628c62fdba2ee23590c107057e83c41
sunerok <justinvforvendetta@gmail.com>
2024-11-23 13:30:19 -0500

    Update boost.mk

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -1,20 +1,13 @@
 package=boost
-$(package)_version=1_61_0
-$(package)_download_path=https://archives.boost.io/release/1.61.0/source/
+$(package)_version=1_59_0
+$(package)_download_path=http://sourceforge.net/projects/boost/files/boost/1.59.0
 $(package)_file_name=$(package)_$($(package)_version).tar.bz2
-$(package)_sha256_hash=A547BD06C2FD9A71BA1D169D9CF0339DA7EBF4753849A8F7D6FDB8FEEE99B640
-
-$(package)_compiler=
-ifeq ($(CLANG_ARG),true)
-$(package)_compiler=clang
-else
-$(package)_compiler=gcc
-endif
+$(package)_sha256_hash=727a932322d94287b62abb1bd2d41723eec4356a7728909e38adb65ca25241ca
 
 define $(package)_set_vars
 $(package)_config_opts_release=variant=release
 $(package)_config_opts_debug=variant=debug
-$(package)_config_opts=--layout=system
+$(package)_config_opts=--layout=tagged --build-type=complete --user-config=user-config.jam
 $(package)_config_opts+=threading=multi link=static -sNO_BZIP2=1 -sNO_ZLIB=1
 $(package)_config_opts_linux=threadapi=pthread runtime-link=shared
 $(package)_config_opts_darwin=--toolset=darwin-4.2.1 runtime-link=shared
@@ -22,14 +15,13 @@ $(package)_config_opts_mingw32=binary-format=pe target-os=windows threadapi=win3
 $(package)_config_opts_x86_64_mingw32=address-model=64
 $(package)_config_opts_i686_mingw32=address-model=32
 $(package)_config_opts_i686_linux=address-model=32 architecture=x86
-$(package)_toolset_$(host_os)=$($(package)_compiler)
+$(package)_toolset_$(host_os)=gcc
 $(package)_archiver_$(host_os)=$($(package)_ar)
 $(package)_toolset_darwin=darwin
 $(package)_archiver_darwin=$($(package)_libtool)
 $(package)_config_libraries=chrono,filesystem,date_time,program_options,iostreams,system,thread,test
-$(package)_cxxflags=-std=c++11 -fvisibility=hidden
+$(package)_cxxflags=-fvisibility=hidden
 $(package)_cxxflags_linux=-fPIC
-$(package)_cxxflags_freebsd=-fPIC
 endef
 
 define $(package)_config_cmds
