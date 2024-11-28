e47eaaf3391a990c035d96dea82d13e2c05bdaaa
sunerok <justinvforvendetta@gmail.com>
2024-11-27 21:49:35 -0500

    edit openssl.mk

--- depends/packages/openssl.mk
+++ depends/packages/openssl.mk
@@ -6,30 +6,19 @@ $(package)_sha256_hash=186c6bfe6ecfba7a5b48c47f8a1673d0f3b0e5ba2e25602dd23b62997
 
 define $(package)_set_vars
 $(package)_config_env=AR="$($(package)_ar)" RANLIB="$($(package)_ranlib)" CC="$($(package)_cc)"
-$(package)_config_opts=--prefix=$(host_prefix) --openssldir=$(host_prefix)/etc/openssl
-$(package)_config_opts+=no-dso
-$(package)_config_opts+=no-shared
+$(package)_config_opts=--prefix=$(host_prefix) --openssldir=$(host_prefix)/etc/openssl no-zlib no-shared no-dso
+$(package)_config_opts+=no-capieng no-cast no-cms no-dtls1 no-gost no-heartbeats no-idea no-md2
+$(package)_config_opts+=no-mdc2 no-rc5 no-rdrand no-rfc3779 no-sctp no-seed no-whirlpool no-rc2 no-rc4 no-ssl2 no-ssl3
 $(package)_config_opts+=$($(package)_cflags) $($(package)_cppflags)
-$(package)_config_opts_linux=-fPIC -Wa,--noexecstack
+$(package)_config_opts_linux=-fPIC
 $(package)_config_opts_x86_64_linux=linux-x86_64
 $(package)_config_opts_i686_linux=linux-generic32
 $(package)_config_opts_arm_linux=linux-generic32
-$(package)_config_opts_armv7l_linux=linux-generic32
-$(package)_config_opts_aarch64_linux=linux-generic64
-$(package)_config_opts_mipsel_linux=linux-generic32
-$(package)_config_opts_mips_linux=linux-generic32
-$(package)_config_opts_powerpc_linux=linux-generic32
-$(package)_config_opts_riscv32_linux=linux-generic32
-$(package)_config_opts_riscv64_linux=linux-generic64
 $(package)_config_opts_x86_64_darwin=darwin64-x86_64-cc
 $(package)_config_opts_x86_64_mingw32=mingw64
 $(package)_config_opts_i686_mingw32=mingw
 endef
 
-define $(package)_preprocess_cmds
-  sed -i 's/#define DATE.*/#define DATE "built on: date not available"/g' util/mkbuildinf.pl && \
-  sed -i '/$config{dirs}/{s/"apps", "test", //g}' Configure
-endef
 
 define $(package)_config_cmds
   ./Configure $($(package)_config_opts)
