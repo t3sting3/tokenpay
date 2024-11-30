51ef1b51607c6402e16fbb5475ffcda4d3118421
sunerok <justinvforvendetta@gmail.com>
2024-11-30 15:41:13 -0500

    fix icu depends

--- depends/packages/icu4c.mk
+++ depends/packages/icu4c.mk
@@ -1,32 +1,39 @@
-package=icu4c
-$(package)_version=55.1
-$(package)_download_path=https://github.com/stoffu/depends-sources/releases/download/v1/
-$(package)_file_name=icu4c-55.1.tar.gz
-$(package)_sha256_hash=1f912c54035533fb4268809701d65c7468d00e292efbc31e6444908450cc46ef
-$(package)_patches=icu-001-dont-build-static-dynamic-twice.patch
+package=icu
+$(package)_version=58.1
+$(package)_download_path=https://sourceforge.net/projects/icu/files/ICU4C/58.1/icu4c-58_1-src.tgz/download
+$(package)_file_name=icu4c-58_1-src.tgz
+$(package)_sha256_hash=0EB46BA3746A9C2092C8AD347A29B1A1B4941144772D13A88667A7B11EA30309
+$(package)_build_subdir=source
+
+
+$(package)_patch_xlocale = sed -i 's|xlocale[.]h|locale.h|' i18n/digitlst.cpp
+$(package)_reverse_patch_xlocale_linux = true
+$(package)_reverse_patch_xlocale_mingw32 = true
+$(package)_reverse_patch_xlocale_darwin = sed -i 's|locale[.]h|xlocale.h|' i18n/digitlst.cpp
+$(package)_reverse_patch_xlocale =  $($(package)_reverse_patch_xlocale_$(host_os))
+
 
 define $(package)_set_vars
-  $(package)_build_opts=CFLAGS="$($(package)_cflags) $($(package)_cppflags) -DU_USING_ICU_NAMESPACE=0 --std=gnu++0x -DU_STATIC_IMPLEMENTATION -DU_COMBINED_IMPLEMENTATION -fPIC"
+$(package)_config_opts=
 endef
 
 define $(package)_config_cmds
-  patch -p1 < $($(package)_patch_dir)/icu-001-dont-build-static-dynamic-twice.patch &&\
-  mkdir builda &&\
-  mkdir buildb &&\
-  cd builda &&\
-  sh ../source/runConfigureICU Linux &&\
-  make &&\
-  cd ../buildb &&\
-  sh ../source/$($(package)_autoconf) --enable-static=yes --enable-shared=yes --disable-layoutex --prefix=$(host_prefix) --with-cross-build=`pwd`/../builda &&\
-  $(MAKE) $($(package)_build_opts)
+  $($(package)_patch_xlocale) && \
+  mkdir native && cd native && \
+  PATH=$(or $(HOST_NATIVE_PATH),${PATH}) ../configure && \
+  PATH=$(or $(HOST_NATIVE_PATH),${PATH}) make -j 10 && cd .. && \
+  $($(package)_reverse_patch_xlocale) && \
+  PATH=${PATH} $($(package)_autoconf) --with-cross-build=$($(package)_build_dir)/native --prefix=$(host_prefix) --enable-extras=no \
+  --enable-strict=no -enable-shared=yes --enable-tests=no \
+  --enable-samples=no --enable-dyload=no --enable-tools=no CXXFLAGS="--std=c++11"
 endef
 
-#define $(package)_build_cmds
-#  cd source &&\
-   $(MAKE) $($((package)_build_opts) `nproc`
-#endef
+define $(package)_build_cmds
+  sed -i.old 's|gCLocale = _create_locale(LC_ALL, "C");|gCLocale = NULL;|' i18n/digitlst.cpp && \
+  sed -i.old "s|freelocale(gCLocale);||" i18n/digitlst.cpp && \
+  PATH=${PATH} $(MAKE)
+endef
 
 define $(package)_stage_cmds
-  cd buildb &&\
-  $(MAKE) $($(package)_build_opts) DESTDIR=$($(package)_staging_dir) install lib/*
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install $($(package)_build_opts)
 endef
