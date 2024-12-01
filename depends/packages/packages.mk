6abc130246cc63c5674f06e90b7be391a2bf8055
sunerok <justinvforvendetta@gmail.com>
2024-12-01 11:54:15 -0500

    Update packages.mk

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,4 +1,4 @@
-packages:=boost openssl libevent zeromq libseccomp libcap
+packages:=boost openssl libevent zeromq libseccomp libcap icu4c
 native_packages := native_ccache
 
 qt_native_packages = native_protobuf native_gperf
