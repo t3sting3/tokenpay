3552fff5e10e30d6df089408cbf60b7c8ff4de1c
sunerok <justinvforvendetta@gmail.com>
2024-12-01 09:51:37 -0500

    Update qt.mk

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -88,7 +88,6 @@ $(package)_config_opts_linux += -qt-xcb
 $(package)_config_opts_linux += -system-freetype
 $(package)_config_opts_linux += -no-feature-sessionmanager
 $(package)_config_opts_linux += -fontconfig
-$(package)_config_opts_linux += -no-opengl
 $(package)_config_opts_linux += -dbus-runtime
 
 $(package)_config_opts_arm_linux += -platform linux-g++ -xplatform bitcoin-linux-g++
