5ac2f62bd231f967b7a7df54a8bbd60ccf278454
sunerok <justinvforvendetta@gmail.com>
2024-12-01 13:25:28 -0500

    updates

--- depends/packages/openssl.mk
+++ depends/packages/openssl.mk
@@ -1,17 +1,20 @@
 package=openssl
-$(package)_version=1.1.1s
+$(package)_version=1.1.1w
 $(package)_download_path=https://www.openssl.org/source
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=c5ac01e760ee6ff0dab61d6b2bbd30146724d063eb322180c6f18a6f74e4b6aa
+$(package)_sha256_hash=cf3098950cb4d853ad95c0841f1f9c6d3dc102dccfcacd521d93925208b76ac8
 
 define $(package)_set_vars
 $(package)_config_env=AR="$($(package)_ar)" RANLIB="$($(package)_ranlib)" CC="$($(package)_cc)"
 $(package)_config_opts=--prefix=$(host_prefix) --openssldir=$(host_prefix)/etc/openssl
 $(package)_config_opts+=no-dso
 $(package)_config_opts+=no-shared
+$(package)_config_opts+=no-ssl-trace
+$(package)_config_opts+=no-ssl2
+$(package)_config_opts+=no-ssl3
+$(package)_config_opts+=no-tests
+$(package)_config_opts+=no-unit-test
 $(package)_config_opts+=no-weak-ssl-ciphers
-$(package)_config_opts+=no-zlib
-$(package)_config_opts+=no-zlib-dynamic
 $(package)_config_opts+=$($(package)_cflags) $($(package)_cppflags)
 $(package)_config_opts_linux=-fPIC -Wa,--noexecstack
 $(package)_config_opts_x86_64_linux=linux-x86_64
@@ -35,18 +38,14 @@ define $(package)_preprocess_cmds
 endef
 
 define $(package)_config_cmds
-  ./Configure $($(package)_config_opts)
+  ./Configure $($(package)_config_opts) && \
+  $(MAKE) depend
 endef
 
 define $(package)_build_cmds
   $(MAKE) -j1 build_libs libcrypto.pc libssl.pc openssl.pc
 endef
 
-# 
 define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) -j1 install_dev
-endef
-
-define $(package)_postprocess_cmds
-  rm -rf share bin etc
+  $(MAKE) DESTDIR=$($(package)_staging_dir) -j1 install_sw
 endef
--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,8 +1,8 @@
-packages:=boost openssl libevent zeromq libseccomp libcap icu4c
+packages:=boost openssl libevent zeromq libseccomp libcap
 native_packages := native_ccache
 
 qt_native_packages = native_protobuf native_gperf
-qt_packages = qrencode protobuf zlib
+qt_packages = qrencode protobuf zlib icu
 
 qt_x86_64_linux_packages:=qt expat dbus libxcb xcb_proto libXau xorgproto xproto freetype fontconfig libX11 xextproto libXext xtrans libXrender renderproto native_gperf
 qt_i686_linux_packages:=$(qt_x86_64_linux_packages)
