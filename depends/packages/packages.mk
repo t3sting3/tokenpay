37d864a2ae1940d1c744de3e82e35ca38080c93f
Reynardo Etenia Wongso <reynardoew@gmail.com>
2024-12-28 14:58:30 +0700

    fix patches

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -4,7 +4,7 @@ native_packages := native_ccache
 qt_native_packages = native_protobuf native_gperf
 qt_packages = qrencode protobuf openssl icu
 
-qt_x86_64_linux_packages:=qt expat dbus libxcb xcb_proto libXau xorgproto xauorg xproto freetype fontconfig libX11 xextproto libXext xtrans libXrender renderproto
+qt_x86_64_linux_packages:=qt expat dbus libxcb xcb_proto libXau xorgproto xproto freetype fontconfig libX11 xextproto libXext xtrans libXrender renderproto
 qt_i686_linux_packages:=$(qt_x86_64_linux_packages)
 
 qt_darwin_packages=qt
