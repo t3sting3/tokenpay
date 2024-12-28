7252debdbf7dedc5776aa59a39416a9a5beae82c
Reynardo Etenia Wongso <reynardoew@gmail.com>
2024-12-28 14:08:31 +0700

    test modifying qt build

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -2,9 +2,9 @@ packages:=boost openssl libevent zeromq libseccomp libcap
 native_packages := native_ccache
 
 qt_native_packages = native_protobuf native_gperf
-qt_packages = qrencode protobuf zlib icu
+qt_packages = qrencode protobuf openssl icu
 
-qt_x86_64_linux_packages:=qt expat dbus libxcb xcb_proto libXau xorgproto xproto freetype fontconfig libX11 xextproto libXext xtrans libXrender renderproto native_gperf
+qt_x86_64_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans libXrender renderproto
 qt_i686_linux_packages:=$(qt_x86_64_linux_packages)
 
 qt_darwin_packages=qt
--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -4,20 +4,19 @@ $(package)_download_path=http://download.qt.io/new_archive/qt/5.7/$($(package)_v
 $(package)_suffix=opensource-src-$($(package)_version).tar.gz
 $(package)_file_name=qtbase-$($(package)_suffix)
 $(package)_sha256_hash=95f83e532d23b3ddbde7973f380ecae1bac13230340557276f75f2e37984e410
-$(package)_dependencies=zlib icu native_gperf
+$(package)_dependencies=pcre libpng icu harfbuzz native_gperf
 $(package)_linux_dependencies=freetype fontconfig libxcb libX11 xproto libXext libXrender renderproto
 $(package)_build_subdir=qtbase
 $(package)_qt_libs=corelib network widgets gui plugins testlib sql concurrent printsupport
 $(package)_patches=mac-qmake.conf mingw-uuidof.patch pidlist_absolute.patch fix-xcb-include-order.patch
-$(package)_patches+=0007-Include-intrin.h-for-declaration-of-_mm_mfence.patch
+# $(package)_patches+=0007-Include-intrin.h-for-declaration-of-_mm_mfence.patch
 $(package)_patches+=strip_log2f.patch
-$(package)_patches+=fix_qt_pkgconfig.patch fix-cocoahelpers-macos.patch qfixed-coretext.patch
-$(package)_patches+=fix_mojave_fonts.patch fix_qt_configure.patch
+# $(package)_patches+=fix_qt_pkgconfig.patch fix-cocoahelpers-macos.patch qfixed-coretext.patch
+# $(package)_patches+=fix_mojave_fonts.patch fix_qt_configure.patch
 $(package)_patches += makegrammar.patch
 $(package)_patches += WebKit1PatchX11.patch
 $(package)_patches += WebKit2TargetPatchX11.patch
 $(package)_patches += widgetsapiPatchX11.patch
-
 
 $(package)_qttranslations_file_name=qttranslations-$($(package)_suffix)
 $(package)_qttranslations_sha256_hash=3a15aebd523c6d89fb97b2d3df866c94149653a26d27a00aac9b6d3020bc5a1d
@@ -43,15 +42,14 @@ $(package)_extra_sources += $($(package)_qtwebkit_file_name)
 $(package)_ssl_extras_mingw32 =-lwsock32 -lgdi32
 $(package)_ssl_extras = $($(package)_ssl_extras_$(host_os))
 
-#Work around for a mingw issue where the prl files contain .a instead of .dll even though we are a shared build - this causes qwt to link the wrong thing and fail.
-$(package)_patch_prl_files_mingw32 = sed -rie 's|lib(.*?)[.]a|\1.dll|' ../qtbase/lib/*.prl && sed -rie 's|(.*?)[.]a|\1.dll|' ../qtbase/lib/*.prl &&
-$(package)_patch_prl_files = $($(package)_patch_prl_files_$(host_os))
+# #Work around for a mingw issue where the prl files contain .a instead of .dll even though we are a shared build - this causes qwt to link the wrong thing and fail.
+# $(package)_patch_prl_files_mingw32 = sed -rie 's|lib(.*?)[.]a|\1.dll|' ../qtbase/lib/*.prl && sed -rie 's|(.*?)[.]a|\1.dll|' ../qtbase/lib/*.prl &&
+# $(package)_patch_prl_files = $($(package)_patch_prl_files_$(host_os))
 
 define $(package)_set_vars
 $(package)_config_opts_release = -release
 $(package)_config_opts_debug = -debug
 $(package)_config_opts += -bindir $(build_prefix)/bin
-$(package)_config_opts += -c++std c++11
 $(package)_config_opts += -confirm-license
 $(package)_config_opts += -dbus-runtime
 $(package)_config_opts += -hostprefix $(build_prefix)
@@ -92,17 +90,17 @@ $(package)_config_opts += -nomake tests
 $(package)_config_opts += -opensource
 $(package)_config_opts += -no-openssl
 $(package)_config_opts += -optimized-qmake
-$(package)_config_opts += -no-pch
+$(package)_config_opts += -pch
 $(package)_config_opts += -pkg-config
 $(package)_config_opts += -prefix $(host_prefix)
-$(package)_config_opts += -qt-harfbuzz
 $(package)_config_opts += -qt-libpng
 $(package)_config_opts += -qt-libjpeg
-$(package)_config_opts += -system-zlib
+$(package)_config_opts += -qt-pcre
+$(package)_config_opts += -qt-zlib
 $(package)_config_opts += -reduce-exports
-$(package)_config_opts += -no-use-gold-linker
 $(package)_config_opts += -silent
 $(package)_config_opts += -v
+
 
 ifneq ($(build_os),darwin)
 $(package)_config_opts_darwin = -xplatform macx-clang-linux
@@ -123,8 +121,6 @@ $(package)_config_opts_linux += -no-opengl
 $(package)_config_opts_arm_linux  = -platform linux-g++ -xplatform $(host)
 $(package)_config_opts_i686_linux  = -xplatform linux-g++-32
 $(package)_config_opts_mingw32  = -no-opengl -xplatform win32-g++ -device-option CROSS_COMPILE="$(host)-"
-$(package)_cppflags += $(shell pkg-config --cflags libxml-2.0)
-$(package)_cppflags += $(shell pkg-config --cflags libxslt)
 $(package)_build_env  = QT_RCC_TEST=1
 endef
 
@@ -168,21 +164,15 @@ define $(package)_preprocess_cmds
   cp -f $($(package)_patch_dir)/mac-qmake.conf qtbase/mkspecs/macx-clang-linux/qmake.conf && \
   patch -p1 < $($(package)_patch_dir)/mingw-uuidof.patch && \
   patch -p1 < $($(package)_patch_dir)/pidlist_absolute.patch && \
-  patch -p1 < $($(package)_patch_dir)/fix_qt_configure.patch && \
   patch -p1 < $($(package)_patch_dir)/fix-xcb-include-order.patch && \
   patch -p1 < $($(package)_patch_dir)/fix_qt_pkgconfig.patch && \
-  patch -p1 < $($(package)_patch_dir)/fix-cocoahelpers-macos.patch && \
-  patch -p1 < $($(package)_patch_dir)/qfixed-coretext.patch && \
-  patch -p1 < $($(package)_patch_dir)/0007-Include-intrin.h-for-declaration-of-_mm_mfence.patch && \
-  patch -p1 < $($(package)_patch_dir)/fix_mojave_fonts.patch && \
   $($(package)_patch_glibc_compat) \
-  echo "!host_build: QMAKE_CFLAGS     += $($(package)_cflags) $($(package)_cppflags)" >> qtbase/mkspecs/common/gcc-base.conf && \
-  echo "!host_build: QMAKE_CXXFLAGS   += $($(package)_cxxflags) $($(package)_cppflags)" >> qtbase/mkspecs/common/gcc-base.conf && \
-  echo "!host_build: QMAKE_LFLAGS     += $($(package)_ldflags)" >> qtbase/mkspecs/common/gcc-base.conf && \
-  sed -i.old "s|QMAKE_CFLAGS            = |!host_build: QMAKE_CFLAGS            = $($(package)_cflags) $($(package)_cppflags) |" qtbase/mkspecs/win32-g++/qmake.conf && \
-  sed -i.old "s|QMAKE_LFLAGS            = |!host_build: QMAKE_LFLAGS            = $($(package)_ldflags) |" qtbase/mkspecs/win32-g++/qmake.conf && \
-  sed -i.old "s|QMAKE_CXXFLAGS          = |!host_build: QMAKE_CXXFLAGS            = $($(package)_cxxflags) $($(package)_cppflags) |" qtbase/mkspecs/win32-g++/qmake.conf && \
-  sed -i.old "s|debug_and_release|release|g" qtbase/mkspecs/win32-g++/qmake.conf && \
+  echo "QMAKE_CFLAGS     += $($(package)_cflags) $($(package)_cppflags)" >> qtbase/mkspecs/common/gcc-base.conf && \
+  echo "QMAKE_CXXFLAGS   += $($(package)_cxxflags) $($(package)_cppflags)" >> qtbase/mkspecs/common/gcc-base.conf && \
+  echo "QMAKE_LFLAGS     += $($(package)_ldflags)" >> qtbase/mkspecs/common/gcc-base.conf && \
+  sed -i.old "s|QMAKE_CFLAGS            = |QMAKE_CFLAGS            = $($(package)_cflags) $($(package)_cppflags) |" qtbase/mkspecs/win32-g++/qmake.conf && \
+  sed -i.old "s|QMAKE_LFLAGS            = |QMAKE_LFLAGS            = $($(package)_ldflags) |" qtbase/mkspecs/win32-g++/qmake.conf && \
+  sed -i.old "s|QMAKE_CXXFLAGS          = |QMAKE_CXXFLAGS            = $($(package)_cxxflags) $($(package)_cppflags) |" qtbase/mkspecs/win32-g++/qmake.conf
   patch -p1 < $($(package)_patch_dir)/makegrammar.patch && \
   patch -p1 < $($(package)_patch_dir)/WebKit1PatchX11.patch && \
   patch -p1 < $($(package)_patch_dir)/WebKit2TargetPatchX11.patch && \
@@ -193,35 +183,27 @@ define $(package)_config_cmds
   export PKG_CONFIG_SYSROOT_DIR=/ && \
   export PKG_CONFIG_LIBDIR=$(host_prefix)/lib/pkgconfig && \
   export PKG_CONFIG_PATH=$(host_prefix)/share/pkgconfig  && \
-  sed -i.old "s|LIBS_PRIVATE += -lz|LIBS_PRIVATE += `pkg-config zlib --libs`|" src/3rdparty/zlib_dependency.pri && \
+  PKG_CONFIG_PATH='$(host_prefix)/share/pkgconfig' OPENSSL_LIBS='-L$(host_prefix)/lib -lssl -lcrypto $($(package)_ssl_extras)' ./configure `pkg-config icu-i18n icu-uc --cflags` `pkg-config icu-i18n icu-uc --libs` $($(package)_config_opts) && \
   OPENSSL_LIBS='-L$(host_prefix)/lib -lssl -lcrypto $($(package)_ssl_extras)' ./configure `pkg-config icu-i18n icu-uc --cflags` `pkg-config icu-i18n icu-uc --libs` $($(package)_config_opts) && \
-  echo "host_build: QT_CONFIG ~= s/system-zlib/zlib" >> mkspecs/qconfig.pri && \
-  echo "CONFIG += force_bootstrap" >> mkspecs/qconfig.pri && \
   $(MAKE) sub-src-clean && \
   cd ../qtwebkit && ls -la && SQLITE3SRCDIR="../qtbase/src/3rdparty/sqlite" ../qtbase/bin/qmake WebKit.pro -o Makefile && \
   cd ../qttranslations && ../qtbase/bin/qmake qttranslations.pro -o Makefile && \
-  cd translations && ../../qtbase/bin/qmake translations.pro -o Makefile && cd .. &&\
+  cd translations && ../../qtbase/bin/qmake translations.pro -o Makefile && cd .. && \
   cd ../qttools/src/linguist/lrelease/ && ../../../../qtbase/bin/qmake lrelease.pro -o Makefile
 endef
 
 define $(package)_build_cmds
-  $(MAKE) -C src $(addprefix sub-,$($(package)_qt_libs)) && \
+  $(MAKE) && \
   $(MAKE) -C ../qttools/src/linguist/lrelease && \
   $(MAKE) -C ../qttranslations && \
-  $($(package)_patch_prl_files) \
   $(MAKE) -C ../qtwebkit
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) -C src INSTALL_ROOT=$($(package)_staging_dir) $(addsuffix -install_subtargets,$(addprefix sub-,$($(package)_qt_libs))) && cd .. && \
-  cp qtbase/bin/qmake* $($(package)_staging_prefix_dir)/native/bin/ && \
-  mkdir -p $($(package)_staging_prefix_dir)/plugins/sqldrivers && \
-  mkdir -p $($(package)_staging_prefix_dir)/plugins/printsupport && \
-  mkdir -p $($(package)_staging_prefix_dir)/plugins/platforms && \
-  mkdir -p $($(package)_staging_prefix_dir)/plugins/bearer && \
+  $(MAKE) -C src INSTALL_ROOT=$($(package)_staging_dir) $(addsuffix -install_subtargets,$(addprefix sub-,$($(package)_qt_libs))) && cd .. &&\
+  $(MAKE) -C qtwebkit INSTALL_ROOT=$($(package)_staging_dir) install_subtargets && \
   $(MAKE) -C qttools/src/linguist/lrelease INSTALL_ROOT=$($(package)_staging_dir) install_target && \
   $(MAKE) -C qttranslations INSTALL_ROOT=$($(package)_staging_dir) install_subtargets && \
-  $(MAKE) -C qtwebkit INSTALL_ROOT=$($(package)_staging_dir) install_subtargets && \
   if `test -f qtbase/src/plugins/platforms/xcb/xcb-static/libxcb-static.a`; then \
     cp qtbase/src/plugins/platforms/xcb/xcb-static/libxcb-static.a $($(package)_staging_prefix_dir)/lib; \
   fi
